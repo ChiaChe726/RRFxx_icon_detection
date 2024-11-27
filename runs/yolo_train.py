ccf9e6d843b5c718266fa867ddc00bd311af324e
RRFxx_NB <RRFxx_NB@gmail.com>
2024-11-27 14:35:39 +0800

    Test: OK NB CPU: 13700H GPU: RTX 4060

--- runs/yolo_train.py
+++ runs/yolo_train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 yolo = YOLO("yolov8n.pt")
 
-results = yolo.train(data='./HHE-6/data.yaml', epochs=1, batch=8)
+results = yolo.train(data='datasets/HHE-6/data.yaml', epochs=1, batch=8)
